98fa13568cc9913452e26444f7ff8fbdc2a0ffe2
Michele Tassoni <michele.tassoni@contorion.de>
2023-12-15 16:16:55 +0100

    ch06 code (partial)

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,8 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+// Update the signature for the routes() method so that it returns a http.Handler instead of *http.ServeMux.
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -12,5 +13,9 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return mux
+	// 1. Pass the servemux as the 'next' parameter to the secureHeaders middleware.
+	// Because secureHeaders is just a function, and the function returns a
+	// http.Handler we don't need to do anything else.
+	// 2. Wrap the existing chain with the logRequest middleware.
+	return app.logRequest(secureHeaders(mux))
 }
