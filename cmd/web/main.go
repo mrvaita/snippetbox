b98d6e8cd592091837c89e59f2ddc84216de0b40
michele.tassoni <michele.tassoni@contorion.de>
2023-12-18 21:50:46 +0100

    ch10 code. Complete

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"database/sql"
 	"flag"
 	"html/template"
@@ -17,7 +18,6 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// Add a new sessionManager field to the application struct.
 type application struct {
 	errorLog	*log.Logger
 	infoLog		*log.Logger
@@ -50,15 +50,15 @@ func main() {
 
 	formDecoder := form.NewDecoder()
 
-	// Use the scs.New() function to initialize a new session manager. Then we
-	// configure it to use our MySQL database as the session store, and set a
-	// lifetime of 12 hours (so that sessions automatically expire 12 hours
-	// after first being created).
 	sessionManager := scs.New()
 	sessionManager.Store = mysqlstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
+	// Make sure that the Secure attribute is set on our session cookies.
+	// Setting this means that the cookie will only be sent by a user's web
+	// browser when a HTTPS connection is being used (and won't be sent over an
+	// unsecure HTTP connection).
+	sessionManager.Cookie.Secure = true
 
-	// And add the session manager to our application dependencies.
 	app := &application{
 		errorLog: errorLog,
 		infoLog: infoLog,
@@ -68,14 +68,28 @@ func main() {
 		sessionManager: sessionManager,
 	}
 
+	// Initialize a tls.Config struct to hold the non-default TLS settings we
+	// want the server to use. In this case the only thing that we're changing
+	// is the curve preferences value, so that only elliptic curves with
+	// assembly implementations are used.
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+	}
+
+	// Set the server's TLSConfig field to use the tlsConfig variable we just created
 	srv := &http.Server{
 		Addr: *addr,
 		ErrorLog: errorLog,
 		Handler: app.routes(),
+		TLSConfig: tlsConfig,
+		// Add Idle, Read and Write timeouts to the server.
+		IdleTimeout: time.Minute,
+		ReadTimeout: 5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -13,7 +13,7 @@ type templateData struct {
 	Snippet	    *models.Snippet
 	Snippets    []*models.Snippet
 	Form	    any
-	Flash	    string // Add a Flash field to the templateData struct.
+	Flash	    string
 }
 
 func humanDate(t time.Time) string {
