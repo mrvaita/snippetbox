e82270a1cca37263659d2f68172ad3dae3b914ba
michele.tassoni <michele.tassoni@contorion.de>
2023-12-30 20:14:48 +0100

    ch14 code. Complete

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -37,10 +37,6 @@ var functions = template.FuncMap{
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
-	// Use fs.Glob() to get a slice of all filepaths in the ui.Files embedded
-	// filesystem which match the pattern 'html/pages/*.tmpl'. This essentially
-	// gives us a slice of all the 'page' templates for the application, just
-	// like before.
 	pages, err := fs.Glob(ui.Files, "html/pages/*.tmpl")
 	if err != nil {
 		return nil, err
@@ -49,16 +45,12 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		// Create a slice containing the filepath patterns for the templates we
-		// want to parse.
 		patterns := []string{
 			"html/base.tmpl",
 			"html/partials/*.tmpl",
 			page,
 		}
 
-		// Use ParseFS() instead of ParseFiles() to parse the template files
-		// from the ui.Files embedded filesystem.
 		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
 		if err != nil {
 			return nil, err
--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -44,8 +44,6 @@ func MaxChars(value string, n int) bool {
 	return utf8.RuneCountInString(value) <= n
 }
 
-// Replace PermittedInt() with a generic PermittedValue() function. This returns
-// true if the value of type T equals one of the variadic permittedValues parameters.
 func PermittedValue[T comparable](value T, permittedValues ...T) bool {
 	for i := range permittedValues {
 		if value == permittedValues[i] {
