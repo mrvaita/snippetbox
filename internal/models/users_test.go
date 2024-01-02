ed2f697f93fcbadec3b10e942370f55cf93ac3b6
Michele Tassoni <michele.tassoni@contorion.de>
2024-01-02 14:01:49 +0100

    ch172 code. Complete

--- internal/models/users_test.go
+++ internal/models/users_test.go
@@ -5,12 +5,10 @@ import (
 	"snippetbox.mrvaita.net/internal/assert"
 )
 func TestUserModelExists(t *testing.T) {
-	// Skip the test if the "-short" flag is provided when running the test.
 	if testing.Short() {
 		t.Skip("models: skipping integration test")
 	}
 
-	// Set up a suite of table-driven tests and expected results.
 	tests := []struct {
 		name string
 		userID int
@@ -35,17 +33,10 @@ func TestUserModelExists(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			// Call the newTestDB() helper function to get a connection pool to
-			// our test database. Calling this here -- inside t.Run() -- means
-			// that fresh database tables and data will be set up and torn down
-			// for each sub-test.
 			db := newTestDB(t)
 
-			// Create a new instance of the UserModel.
 			m := UserModel{db}
 
-			// Call the UserModel.Exists() method and check that the return
-			// value and error match the expected values for the sub-test.
 			exists, err := m.Exists(tt.userID)
 			assert.Equal(t, exists, tt.want)
 			assert.NilError(t, err)
